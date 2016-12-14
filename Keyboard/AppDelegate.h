30ba73e899ac22d3cff9f42a3af1f0f4df58cf79
iChenwin <ichenwin@gmail.com>
2016-12-14 10:54:36 +0800

    Initial Commit

---
+++ Keyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Keyboard
+//
+//  Created by wayne on 2016/12/14.
+//  Copyright © 2016年 wayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Keyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Keyboard
+//
+//  Created by wayne on 2016/12/14.
+//  Copyright © 2016年 wayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
